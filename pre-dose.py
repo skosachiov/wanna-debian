162b1731162d2ffb2558d58b21864974d31820cb
Serge Kosachiov <skosachiov@gmail.com>
2025-07-29 13:59:17 +0300

    group rm

--- pre-dose.py
+++ pre-dose.py
@@ -214,7 +214,7 @@ if __name__ == "__main__":
             for p in bin_dict[pkg_name]:
                 print(p)
         elif args.resolve_group and pkg_name != None:
-            if target[pkg_name]["source"] != None:  
+            if pkg_name in target and target[pkg_name]["source"] != None:  
                 for p in group_dict[target[pkg_name]["source"]]:
                     print(p)                
         elif args.depends and pkg_name != None:
