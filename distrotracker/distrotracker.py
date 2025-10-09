a08ef073b3357fc163546d53db63a7fc1d59c38b
Serge Kosachiov <skosachiov@gmail.com>
2025-10-09 14:41:45 +0300

    xz

--- distrotracker/distrotracker.py
+++ distrotracker/distrotracker.py
@@ -430,7 +430,7 @@ def update_metadata(base_url, local_base_dir, dists, components, architectures):
                 download_status = download_file(remote_url, local_z_path)
                 # Download .xz file
                 if file_path.endswith('Packages.gz') and download_status == None:
-                    file_path = file_path.replace('.gz', 'xz')
+                    file_path = file_path.replace('.gz', '.xz')
                     local_z_path = os.path.join(dist_dir, file_path)
                     download_status = download_file(remote_url, local_z_path)
                 if download_status:
