2c69fd6be771a7b9a39626020ab69f4ef34123ba
skosachiov <skosachiov@gmail.com>
2025-11-23 18:08:46 +0300

    fix

--- simplebuilder/__init__.py
+++ simplebuilder/__init__.py
@@ -2,11 +2,13 @@ __version__ = "0.1.0"
 
 # Import and export all functions needed for testing
 from .simplebuilder import (
-    build_from_dsc,
-    build_from_git
+    clone_and_build_gbp,
+    download_and_build_dpkg,
+    copy_to_repo
 )
 
 __all__ = [
-    'build_from_dsc',
-    'build_from_git'
+    "clone_and_build_gbp",
+    "download_and_build_dpkg",
+    "copy_to_repo",
 ]
