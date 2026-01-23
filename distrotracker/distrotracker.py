a23ca9b41a4e794542d9b1c4489b83d777aa6fb4
skosachiov <skosachiov@gmail.com>
2026-01-23 13:53:28 +0300

    force dist

--- distrotracker/distrotracker.py
+++ distrotracker/distrotracker.py
@@ -397,6 +397,8 @@ def update_metadata(base_url, local_base_dir, dists, components, builds, session
             metadata_files.append(build + "/Packages")
 
     for dist in distributions:
+        if dists and dist not in dists:
+            continue
         logging.info(f"Processing distribution: {dist}")
         dist_url = urljoin(base_url, "dists/" + dist + "/")
         dist_dir = os.path.join(local_base_dir, "dists/" + dist)
@@ -408,9 +410,6 @@ def update_metadata(base_url, local_base_dir, dists, components, builds, session
                     file_path = component + "/" + metadata_file + extension
                     remote_url = urljoin(dist_url, file_path)
                     local_z_path = os.path.join(dist_dir, file_path)
-                    if dists and dist not in dists:
-                        download_status = False
-                        break
                     download_status = download_file(remote_url, local_z_path, session)
                     if download_status is not None:
                         break
