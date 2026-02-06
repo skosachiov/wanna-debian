596fc82a3790659d04918c6438ad8a95803b65ec
skosachiov <skosachiov@gmail.com>
2026-02-06 11:30:34 +0300

    suffix

--- simplebuilder/simplebuilder.py
+++ simplebuilder/simplebuilder.py
@@ -64,7 +64,7 @@ def clone_and_build_gbp(repo_url, build_dir, repo_dir):
     # Build with gbp-buildpackage
     if os.environ['LOCALSUFFIX']:
         run_command(f"cd {repo_name}; gbp dch --local {os.environ['LOCALSUFFIX']} 'Add suffix'; \
-            git -c user.name='simplebuilder' -c user.email='simplebuilder@localhost' commit -am 'Add suffix'", \
+            git -c user.name={os.environ['DEBFULLNAME']} -c user.email={os.environ['DEBEMAIL']} commit -am 'Add suffix'", \
             cwd=build_dir)
     
     if run_command("gbp buildpackage -uc -us --git-no-pristine-tar --git-ignore-new --git-export-dir=../build-area", cwd=clone_dir):
