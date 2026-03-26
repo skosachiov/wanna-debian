f1311fa9cd44efdc3e0ce608ede885855ce15933
skosachiov <skosachiov@gmail.com>
2026-03-26 23:45:20 +0300

    sbuild gbp

--- simplebuilder/simplebuilder.py
+++ simplebuilder/simplebuilder.py
@@ -249,7 +249,7 @@ def gbp_build_with_sbuild(repo_url, dist, chroot_name, extra_repositories=None):
         # Using gbp buildpackage with sbuild as the builder
         sbuild_cmd = (
             f"gbp buildpackage --git-no-pristine-tar --git-ignore-new --git-export-dir=../build-area "
-            f"--git-builder=\"chmod -R 777 ../build-area && sudo -u sbuild sbuild "
+            f"--git-builder=\"chmod -R 777 ../* && sudo -u sbuild sbuild "
             f"--dist={dist} "
             f"--chroot-mode=schroot "
             f"--source "
